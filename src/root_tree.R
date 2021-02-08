24df3459fc090ef2d98b9f1a17c1ea2a4518b99e
JC <www.tsai.com@gmail.com>
2021-02-07 22:49:57 -0500

    build tree

--- src/root_tree.R
+++ src/root_tree.R
@@ -4,19 +4,26 @@ pacman::p_load(data.table, igraph, optparse, Matrix)
 option_list = list(
   make_option(c("-i", "--igr"), type="integer", default="1", 
               help="i-th group of roots [default= %default]", metavar="number"),
-  make_option(c("-y", "--year"), type="integer", default="1", 
+  make_option(c("-g", "--group_size"), type="integer", default="1000", 
+              help="i-th group of roots [default= %default]", metavar="number"),
+  make_option(c("-y", "--year"), type="integer", default="1999", 
               help="year [default= %default]", metavar="number")
 ); 
 
 opt_parser = OptionParser(option_list=option_list)
 opt = parse_args(opt_parser)
+source("../../src/utilities.R")
 
 # setup
-group_size <- 1000
-roots <- readRDS(paste0("../output/roots_", opt$year, ".RDS"))
-g <- readRDS(paste0("../output/g_lar_", opt$year, ".RDS"))
-edges <- as_data_frame(g, "edges")
+group_size <- opt$group_size
+roots <- fread(paste0("../output_hpcc/roots_", opt$year, ".csv"))
+roots <- roots$root
+g <- readRDS(paste0("../../../iFindData/data/g_", opt$year, ".RDS"))
+g_lar <- extract_cc(g, 1)
+
+edges <- as_data_frame(g_lar, "edges")
 setDT(edges)
+edges[, weight:=weight/100]
 
 tar_roots <- roots[((opt$igr-1)*group_size+1):min((opt$igr-1)*group_size+group_size, length(roots))]
 
@@ -26,15 +33,15 @@ tar_roots <- roots[((opt$igr-1)*group_size+1):min((opt$igr-1)*group_size+group_s
 #   dist <- dist[!is.na(dist)]
 # })
 
-ret <- NULL
+ret <- vector("list", length(tar_roots))
 for(i in seq_along(tar_roots)) {
-  print(i)
+  if(i %% 100 == 0 ) print(i)
   r <- tar_roots[i]
   step <- 1
   es <- edges[from == r]
   if(nrow(es) == 0) next
   es[is.na(weight), weight := 1]
-  es <- es[,.(root = from, desc = to, weight = weight, x = step, ctrl = (weight >= .5))]
+  es <- es[,.(root = from, desc = to, weight, step = step, ctrl = (weight >= .5))]
   rret <- es
   
   es_nxt <- merge(es, edges, by.x="desc", by.y="from")
@@ -42,9 +49,10 @@ for(i in seq_along(tar_roots)) {
   while(nrow(es_nxt)!=0) {
     step <- step + 1
     es_nxt[is.na(weight.y), weight.y := 1]
+    es_nxt[, step := NULL]
     es_nxt <- es_nxt[,.(root = root, desc = to, 
                         weight = weight.x * weight.y,
-                        x = step, ctrl = (ctrl & weight.y >= .5))]
+                        step = step, ctrl = (ctrl & weight.y >= .5))]
     rret <- rbind(rret, es_nxt)
     es_nxt <- merge(es_nxt, edges, by.x="desc", by.y="from")
     es_nxt <- es_nxt[!(to %in% rret$desc)]
@@ -54,7 +62,7 @@ for(i in seq_along(tar_roots)) {
 
 ret <- rbindlist(ret)
 
-saveRDS(ret, paste0("../output_hpcc/tree_weights_", opt$year, "_", opt$igr, ".RDS"))
+fwrite(ret, paste0("../output_hpcc/tree/", opt$year, "_", opt$igr, ".csv"))
 
 # distances(make_star(10, "out"))
 
--- src/run_root_tree.sh
+++ src/run_root_tree.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
-#$ -N root-tree
-#$ -t 1-298
-#$ -o ../job_output/$JOB_NAME-$JOB_ID-$TASK_ID.log
+#$ -N tree
+#$ -o ../job_output/$JOB_NAME-$JOB_ID.log
 #$ -j y
-#$ -l m_mem_free=4G
+#$ -l m_mem_free=10G
 
+group_size=10000
 
-Rscript --vanilla root_tree.R -y 2012 -i $SGE_TASK_ID
+for year in {1999..2020}
+do
+	taskname="tree-$year"
+    file="../output_hpcc/roots_$year.csv"
+    num_line=$(wc -l $file | awk '{print $1}')
+    nrun=$(((num_line / group_size)+1))
+    echo $nrun
+	qsub -q short.q -N $taskname -t 1-$nrun -o '../job_output/$JOB_NAME-$JOB_ID-$TASK_ID.log' -j y -b y "Rscript --vanilla root_tree.R -g $group_size -i \${SGE_TASK_ID} --year=$year"
+done
